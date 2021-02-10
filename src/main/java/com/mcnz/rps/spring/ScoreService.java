b33e60fee15c1ed48ed06ccf03021369c1b6d2ec
Cameron <me@example.com>
2021-02-09 20:21:42 -0500

    crossorigin

--- src/main/java/com/mcnz/rps/spring/ScoreService.java
+++ src/main/java/com/mcnz/rps/spring/ScoreService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 @RestController
 public class ScoreService {
 
